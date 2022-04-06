7f54563331c7f6242a84321d160954b5e4af11fa
runeweaver-dev <cameron.raw89@gmail.com>
2022-04-06 12:29:14 +0200

    Implements react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import Menu from './components/Menu';
+import About from './components/About';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Menu />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/about" element={ <About /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Menu.tsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import '../styles/Menu.scss';
+import Title from './Title';
+
+function Menu(){
+    return (
+        <div className="Menu">
+            <Title />
+        </div>
+    )
+}
+
+export default Menu;
