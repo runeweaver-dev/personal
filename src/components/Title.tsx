ee9567f3a93210af8d621d23c883d6b244590640
runeweaver-dev <cameron.raw89@gmail.com>
2022-04-06 12:29:14 +0200

    Implements react router

---
+++ src/components/Title.tsx
@@ -0,0 +1,16 @@
+import react from 'react';
+import '../styles/Title.scss';
+
+function Title(){
+    return (
+        <div className="Title">
+            <div className="textArea">
+                <h1>Cameron Raw</h1>
+                <hr />
+                <h2>Full-Stack Developer</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Title;
