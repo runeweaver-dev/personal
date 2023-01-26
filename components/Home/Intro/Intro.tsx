f2024c13eb2326e18e616a4ab5ac3ac431fb82bb
Cameron Raw <cameron.raw89@gmail.com>
2023-01-26 23:33:03 +0100

    Expanded on intro text

--- components/Home/Intro/Intro.tsx
+++ components/Home/Intro/Intro.tsx
@@ -8,7 +8,7 @@ function Intro() {
             <div className={styles.introCard}>
                 <div className={styles.introText}>
                     <h1>Hi, I'm Cameron</h1>
-                    <p>I'm a fullstack developer, currently based in Barcelona, Spain. </p>
+                    <p>I'm a fullstack developer, currently based in Barcelona, Spain. I'm currently putting together my site.</p>
                 </div>
                 <div className={styles.imageContainer}>
                     <Image className={styles.profileImage} src="/img/solsiden.jpeg" height={300} width={300} alt={'A picture of Cameron Raw, a full-stack Web Developer.'} />
